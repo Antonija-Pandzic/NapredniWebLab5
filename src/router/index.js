4eac2d45857b706adc610b9055a18a608bfecdbb
AntonijaPandzic123 <antonija.pandzic@fer.unizg.hr>
2026-01-23 00:16:52 +0100

    Fix routes (no lazy loading)

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '../views/HomeView.vue'
+import TasksView from '../views/TasksView.vue'
+import TaskDetailsView from '../views/TaskDetailsView.vue'
+import AboutView from '../views/AboutView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
 const routes = [
-  { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
-  { path: '/tasks', name: 'tasks', component: () => import('../views/TasksView.vue') },
-  { path: '/task/:id', name: 'task', props: true, component: () => import('../views/TaskDetailsView.vue') },
-  { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
-  { path: '/:pathMatch(.*)*', name: 'notfound', component: () => import('../views/NotFoundView.vue') },
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/tasks', name: 'tasks', component: TasksView },
+  { path: '/task/:id', name: 'task', props: true, component: TaskDetailsView },
+  { path: '/about', name: 'about', component: AboutView },
+  { path: '/:pathMatch(.*)*', name: 'notfound', component: NotFoundView },
 ]
 
 export default createRouter({
